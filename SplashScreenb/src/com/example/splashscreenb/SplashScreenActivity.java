661302faf06104e0884424d6c44e8c6f0ba35c7c
KyzminIlia <i.kyzmin.j@gmail.com>
2014-07-14 16:46:54 +0400

    minor correction 3
    Добавение фрагмента в активити через FragmentTransaction в
    SplashScreenActivity

--- SplashScreenb/src/com/example/splashscreenb/SplashScreenActivity.java
+++ SplashScreenb/src/com/example/splashscreenb/SplashScreenActivity.java
@@ -1,9 +1,9 @@
 package com.example.splashscreenb;
 
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.app.Activity;
 
 public class SplashScreenActivity extends FragmentActivity {
@@ -11,9 +11,10 @@ public class SplashScreenActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.a_splash);		
-		RetainedFragment retainedFragment = (RetainedFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.retainFragment);
+		RetainedFragment retainedFragment = new RetainedFragment();
+		FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+		ft.add(android.R.id.content, retainedFragment);
+		ft.commit();
 		if (retainedFragment.needFinish()) {
 			startActivity(new Intent(this, MainScreenActivity.class));
 			finish();
